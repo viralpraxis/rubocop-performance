24e33c5af0e96cbdca29c27a0bc75237d062e7c9
viralpraxis <iaroslav2k@gmail.com>
2024-10-22 19:15:06 +0300

    [Fix #475] Add new `Performance/StringBytesize` cop.

---
+++ spec/rubocop/cop/performance/string_bytesize_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Performance::StringBytesize, :config do
+  let(:msg) { 'Use `String#bytesize` instead of calculating the size of the bytes array.' }
+
+  it 'registers an offense with `size` method' do
+    expect_offense(<<~RUBY)
+      string.bytes.size
+             ^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      string.bytesize
+    RUBY
+  end
+
+  it 'registers an offense with `length` method' do
+    expect_offense(<<~RUBY)
+      string.bytes.length
+             ^^^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      string.bytesize
+    RUBY
+  end
+
+  it 'registers an offense with `count` method' do
+    expect_offense(<<~RUBY)
+      string.bytes.count
+             ^^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      string.bytesize
+    RUBY
+  end
+
+  it 'registers an offense with string literal' do
+    expect_offense(<<~RUBY)
+      "foobar".bytes.count
+               ^^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      "foobar".bytesize
+    RUBY
+  end
+
+  it 'registers an offense and autocorrects with safe navigation' do
+    expect_offense(<<~RUBY)
+      string&.bytes&.count
+              ^^^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      string&.bytesize
+    RUBY
+  end
+
+  it 'registers an offense and autocorrects with partial safe navigation' do
+    expect_offense(<<~RUBY)
+      string&.bytes.count
+              ^^^^^^^^^^^ #{msg}
+    RUBY
+
+    expect_correction(<<~RUBY)
+      string&.bytesize
+    RUBY
+  end
+
+  it 'does not register an offenses without array size method' do
+    expect_no_offenses(<<~RUBY)
+      string.bytes
+    RUBY
+  end
+
+  it 'does not register an offenses with `bytes` without explicit receiver' do
+    expect_no_offenses(<<~RUBY)
+      bytes.size
+    RUBY
+  end
+end
